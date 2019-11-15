b1a357a6abc1909f6a6d5ab55bf8ebea415c7a28
Naoki Mita <naoki.mita18@gmail.com>
2019-11-14 20:52:55 -0800

    Add format.py

--- format.py
+++ format.py
@@ -1,31 +1,9 @@
-# 118. Pascal's Triangle
-# https://leetcode.com/problems/pascals-triangle/
+# id. Title
+# url
 
-def generate(numRows):
-    initial = 1
-    list = []
 
-    if 0 == numRows:
-        return list
+def functionName(args):
+    return
 
-    list.append([initial])
-
-    for i in range(initial, numRows):
-        child = []
-        for _ in range(0, i+1):
-            child.append(0)
-        list.append(child)
-
-        j = 0
-        for li in list[i-1]:
-            list[i][j] += li
-            list[i][j+1] += li
-            j += 1
-
-    return list
 
 if __name__ == '__main__':
-    print(generate(0))
-    print(generate(1))
-    print(generate(5))
-    print(generate(10))
