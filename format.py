d18ee55450e2b0e05f46a0fa7c99fa9d0a8b854d
Naoki Mita <naoki.mita18@gmail.com>
2019-11-15 17:16:59 -0800

    Merge for arranging format.py

--- format.py
+++ format.py
@@ -2,8 +2,15 @@
 # url
 
 
-def functionName(args):
+def main():
+  solution = Solution()
+  print(solution.functionName())
+
+
+class Solution:
+    def functionName(self, args: str) -> str:
     return
 
 
 if __name__ == '__main__':
+    main()
