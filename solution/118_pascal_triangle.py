b40d16e6c89a1bb6b78174f978a006147bdd56b6
Naoki Mita <naoki.mita18@gmail.com>
2019-11-16 13:42:11 -0800

    Arrange format in 118

--- solution/118_pascal_triangle.py
+++ solution/118_pascal_triangle.py
@@ -1,30 +1,38 @@
 # 118. Pascal's Triangle
 # https://leetcode.com/problems/pascals-triangle/
 
+from typing import List
 
-def generate(numRows):
-    initial = 1
-    list = []
 
-    if 0 == numRows:
+def main():
+    solution = Solution()
+    print(solution.generate(0))
+    print(solution.generate(1))
+    print(solution.generate(5))
+    print(solution.generate(10))
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        initial = 1
+        list = []
+
+        if 0 == numRows:
+            return list
+
+        list.append([initial])
+
+        for i in range(initial, numRows):
+            list.append([0] * (i + 1))
+
+            j = 0
+            for li in list[i - 1]:
+                list[i][j] += li
+                list[i][j + 1] += li
+                j += 1
+
         return list
-
-    list.append([initial])
-
-    for i in range(initial, numRows):
-        list.append([0] * (i + 1))
-
-        j = 0
-        for li in list[i - 1]:
-            list[i][j] += li
-            list[i][j + 1] += li
-            j += 1
-
-    return list
 
 
 if __name__ == '__main__':
-    print(generate(0))
-    print(generate(1))
-    print(generate(5))
-    print(generate(10))
+    main()
