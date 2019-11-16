777dd91c5a1c8caea60866213d7e89609b98ff43
Naoki Mita <naoki.mita18@gmail.com>
2019-11-16 13:38:53 -0800

    Arrange format in 791

--- solution/791_maximum_subarray.py
+++ solution/791_maximum_subarray.py
@@ -2,32 +2,38 @@
 # https://leetcode.com/problems/custom-sort-string/
 
 
-def customSortString(S, T):
+def main():
+    solution = Solution()
+    print(solution.customSortString("cba", "abcd"))
+    print(solution.customSortString("cba", "aafhouvavoabcac"))
 
-    dict = {}
-    for s in S:
-        dict[s] = 0
 
-    not_match = ""
-    for t in T:
-        match = False
+class Solution:
+    def customSortString(self, S: str, T: str) -> str:
+
+        dict = {}
         for s in S:
-            if t == s:
-                dict[t] += 1
-                match = True
-                break
+            dict[s] = 0
 
-        if not match:
-            not_match += t
+        not_match = ""
+        for t in T:
+            match = False
+            for s in S:
+                if t == s:
+                    dict[t] += 1
+                    match = True
+                    break
 
-    str = ""
-    for k, v in dict.items():
-        str += k * v
-    str += not_match
+            if not match:
+                not_match += t
 
-    return str
+        str = ""
+        for k, v in dict.items():
+            str += k * v
+        str += not_match
+
+        return str
 
 
 if __name__ == '__main__':
-    print(customSortString("cba", "abcd"))
-    print(customSortString("cba", "aafhouvavoabcac"))
+    main()
