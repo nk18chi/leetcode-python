37977fe3430d26e43b4927bc9c59b22830b4b12c
Naoki Mita <naoki.mita18@gmail.com>
2019-11-16 17:39:33 -0800

    Can create TreeNode in format.py

--- format.py
+++ format.py
@@ -5,13 +5,45 @@
 
 
 def main():
-    solution = Solution()
-    print(solution.functionName())
+    tree = createTreeNode([5, 5, 5, 1, 1, 5])
+    s = Solution()
+    print(s.functionName())
 
 
 class Solution:
     def functionName(self, args: str) -> str:
         return
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'<{self.val}, {self.left}, {self.right}>'
+
+
+def createTreeNode(list):
+
+    from collections import deque
+
+    data = list
+    n = iter(data)
+    tree = TreeNode(next(n))
+    fringe = deque([tree])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.left = TreeNode(next(n))
+            fringe.append(head.left)
+            head.right = TreeNode(next(n))
+            fringe.append(head.right)
+        except StopIteration:
+            break
+
+    return tree
 
 
 if __name__ == '__main__':
