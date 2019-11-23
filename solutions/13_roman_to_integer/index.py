2c67e48cbd7a3feb5b661cab983819f18046a772
Naoki Mita <naoki.mita18@gmail.com>
2019-11-22 17:21:02 -0800

    Fix comment

--- solutions/13_roman_to_integer/index.py
+++ solutions/13_roman_to_integer/index.py
@@ -1,5 +1,5 @@
-# 12. Integer to Roman
-# https://leetcode.com/problems/integer-to-roman/
+# 13. Roman to Integer - Easy
+# https://leetcode.com/problems/roman-to-integer/
 
 # from typing import List
 
