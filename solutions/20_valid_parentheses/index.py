90cee729d4b31a8ca702152dadc22e49c21799aa
Naoki Mita <naoki.mita18@gmail.com>
2019-12-13 18:53:27 -0800

    Add 20. Valid Parentheses

---
+++ solutions/20_valid_parentheses/index.py
@@ -0,0 +1,27 @@
+# 20. Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses/
+
+# from typing import List
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+
+        dict = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        stack = []
+        for c in s:
+            if c in dict.values():
+                stack.append(c)
+            elif c in dict.keys():
+                if not stack or dict[c] != stack.pop():
+                    return False
+            else:
+                return False
+
+        return len(stack) == 0
