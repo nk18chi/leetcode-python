5e3755d1e0dcfc216f793541feee963b652357cc
Naoki Mita <naoki.mita18@gmail.com>
2019-12-23 16:24:49 -0800

    Add 1030. Matrix Cells in Distance Order

---
+++ solutions/1030_matrix_cells_in_distance_order/index.py
@@ -0,0 +1,57 @@
+# 1030. Matrix Cells in Distance Order
+# https://leetcode.com/problems/matrix-cells-in-distance-order/
+
+from typing import List
+
+# Input: R = 1, C = 2, r0 = 0, c0 = 0
+# Output: [[0,0],[0,1]]
+# Explanation: The distances from (r0, c0) to other cells are: [0,1]
+
+
+class Solution:
+    def allCellsDistOrder(self, R: int, C: int, r0: int,
+                          c0: int) -> List[List[int]]:
+        # first solution
+        # res, order = [], []
+        # for r in range(R):
+        #     for c in range(C):
+        #         res += [[r, c]]
+        #         order += [abs(r - r0) + abs(c - c0)]
+        # order, res = zip(*sorted(zip(order, res)))
+        # return list(res)
+
+        # second solution
+        # def dfs(self, row, col):
+        #     if row < 0 or row >= R or col < 0 or col >= C:
+        #         return
+
+        #     if (row, col) in checked:
+        #         return
+        #     checked.add((row, col))
+
+        #     distance = abs(row - r0) + abs(col - c0)
+        #     if distance in dict:
+        #         dict[distance] += [[row, col]]
+        #     else:
+        #         dict[distance] = [[row, col]]
+
+        #     dfs(self, row + 1, col)
+        #     dfs(self, row - 1, col)
+        #     dfs(self, row, col + 1)
+        #     dfs(self, row, col - 1)
+
+        # dict, checked = {}, set()
+        # dfs(self, r0, c0)
+        # result = []
+        # for n in range(max(dict.keys()) + 1):
+        #     result += dict[n]
+
+        # return result
+
+        # third solution
+        res = []
+        for r in range(R):
+            for c in range(C):
+                res += [[r, c]]
+
+        return sorted(res, key=lambda l: abs(r0 - l[0]) + abs(c0 - l[1]))
---
+++ solutions/1030_matrix_cells_in_distance_order/test.py
@@ -0,0 +1,25 @@
+import unittest
+import importlib
+f = importlib.import_module(
+    'solutions.1030_matrix_cells_in_distance_order.index')
+
+
+class Test(unittest.TestCase):
+    def test_allCellsDistOrder(self):
+        test_patterns = [
+            (2, 3, 1, 2, [[1, 2], [0, 2], [1, 1], [0, 1], [1, 0], [0, 0]]),
+            # (2, 3, 1, 2, [[1, 2], [0, 2], [1, 1], [0, 1], [1, 0], [0, 0]]),
+            # (2, 2, 0, 1, [[0, 1], [0, 0], [1, 1], [1, 0]]),
+            # (1, 2, 0, 0, [[0, 0], [0, 1]]),
+        ]
+
+        for i, (arg1, arg2, arg3, arg4, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = f.Solution()
+                self.assertEqual(
+                    s.allCellsDistOrder(
+                        arg1, arg2, arg3, arg4), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
