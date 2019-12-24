e17887d056e0447297c3af2587e4bc701c6c0fb8
Naoki Mita <naoki.mita18@gmail.com>
2019-12-23 16:25:18 -0800

    Add 633. Sum of Square Numbers

---
+++ solutions/633_sum_of_square_numbers/index.py
@@ -0,0 +1,13 @@
+# 633. Sum of Square Numbers
+# https://leetcode.com/problems/sum-of-square-numbers/
+
+# from typing import List
+
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        import math
+        for i in range(int(math.sqrt(c)) + 1):
+            if math.sqrt(c - i**2).is_integer():
+                return True
+        return False
