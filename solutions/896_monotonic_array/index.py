8d4d23ad73320ebb17194c13c683b99c9969d719
Naoki Mita <naoki.mita18@gmail.com>
2020-01-11 14:57:38 -0800

    Add 896. Monotonic Array

---
+++ solutions/896_monotonic_array/index.py
@@ -0,0 +1,37 @@
+# 896. Monotonic Array
+# https://leetcode.com/problems/monotonic-array/
+
+from typing import List
+
+
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        # # first solution
+        # flag = 0
+        # for i in range(len(A) - 1):
+        #     if A[i] == A[i + 1]:
+        #         continue
+
+        #     if A[i] < A[i + 1]:
+        #         if flag == -1:
+        #             return False
+        #         flag = 1
+        #     elif A[i] > A[i + 1]:
+        #         if flag == 1:
+        #             return False
+        #         flag = -1
+
+        # return True
+
+        # second solution
+        isGreat = False
+        isLess = False
+        for i in range(1, len(A)):
+            if A[i - 1] < A[i]:
+                isGreat = True
+            elif A[i - 1] > A[i]:
+                isLess = True
+
+            if isGreat and isLess:
+                return False
+            return True
