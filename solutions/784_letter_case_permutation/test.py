2e71f46c3543ef6e070598aa6f5adb2abe681f8d
Naoki Mita <naoki.mita18@gmail.com>
2020-01-31 16:22:10 -0800

    Fix test case of 784

--- solutions/784_letter_case_permutation/test.py
+++ solutions/784_letter_case_permutation/test.py
@@ -6,7 +6,7 @@ f = importlib.import_module('solutions.784_letter_case_permutation.index')
 class Test(unittest.TestCase):
     def test_letterCasePermutation(self):
         test_patterns = [
-            ("a1b2", ["a1b2", "a1B2", "A1b2", "A1B2"]),
+            ("a1b2", ['a1b2', 'A1b2', 'a1B2', 'A1B2'])
         ]
 
         for i, (arg, expected) in enumerate(test_patterns):
