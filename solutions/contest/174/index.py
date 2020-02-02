5b1d45dd3493de8295e2fc15211a33f08dde50e8
Naoki Mita <naoki.mita18@gmail.com>
2020-02-01 22:48:24 -0800

    Add weekly contest 174

---
+++ solutions/contest/174/index.py
@@ -0,0 +1,81 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'<{self.val}, {self.left}, {self.right}>'
+
+
+def createTreeNode(list):
+
+    from collections import deque
+
+    data = list
+    n = iter(data)
+    tree = TreeNode(next(n))
+    fringe = deque([tree])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.left = TreeNode(next(n))
+            fringe.append(head.left)
+            head.right = TreeNode(next(n))
+            fringe.append(head.right)
+        except BaseException:
+            break
+
+    return tree
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        list = []
+        for i, m in enumerate(mat):
+            list.append((i, sum(m)))
+        list.sort(key=lambda x: x[1])
+        res = []
+        for i in range(k):
+            res.append(list[i][0])
+        return res
+
+    def minSetSize(self, arr: List[int]) -> int:
+        target = len(arr) // 2
+        dict = {}
+        for a in arr:
+            dict[a] = dict.get(a, 0) + 1
+        d = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+        i = 1
+        sum = 0
+        for v in d:
+            sum += v[1]
+            if sum >= target:
+                return i
+            i += 1
+
+    def maxProduct(self, root: TreeNode) -> int:
+        self.answer = []
+
+        def helper(self, root):
+            if root is None:
+                return 0
+            elif root.left is None and root.right is None:
+                self.answer.append(root.val)
+                return root.val
+            else:
+                left = helper(self, root.left)
+                right = helper(self, root.right)
+                self.answer.append(root.val + left + right)
+
+                return root.val + left + right
+
+        helper(self, root)
+        _max = max(self.answer)
+        target = 0
+        for a in self.answer:
+            target = max(target, a * (_max - a))
+        return target % (10**9 + 7)
---
+++ solutions/contest/174/test.py
@@ -0,0 +1,48 @@
+import unittest
+import importlib
+f = importlib.import_module('solutions.contest.174.index')
+
+
+class Test(unittest.TestCase):
+    def test_kWeakestRows(self):
+        test_patterns = [
+            ([[1, 1, 0, 0, 0],
+              [1, 1, 1, 1, 0],
+                [1, 0, 0, 0, 0],
+                [1, 1, 0, 0, 0],
+                [1, 1, 1, 1, 1]], 3, [2, 0, 3])
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = f.Solution()
+                self.assertEqual(s.kWeakestRows(arg1, arg2), expected)
+
+    def test_minSetSize(self):
+        test_patterns = [
+            ([3, 3, 3, 3, 5, 5, 5, 2, 2, 7], 2),
+            ([7, 7, 7, 7, 7, 7], 1),
+            ([1, 9], 1),
+            ([1000, 1000, 3, 7], 1),
+            ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = f.Solution()
+                self.assertEqual(s.minSetSize(arg1), expected)
+
+    def test_maxProduct(self):
+        test_patterns = [
+            ([1, 2, 3, 4, 5, 6, 7], 192),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = f.Solution()
+                tree = f.createTreeNode(arg1)
+                self.assertEqual(s.maxProduct(tree), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
