d5fde7f40fd86b0246ae69f4f80eac89eab92f8b
Naoki Mita <naoki.mita18@gmail.com>
2020-02-18 17:44:06 -0800

    Refactoring 977. Squares of a Sorted Array

--- solutions/squares_of_a_sorted_array/index.py
+++ solutions/squares_of_a_sorted_array/index.py
@@ -5,25 +5,25 @@ from typing import List
 
 
 class Solution:
+    # first solution
     def sortedSquares(self, A: List[int]) -> List[int]:
-        # first solution
-        # list = []
-        # for a in A:
-        #     list.append(a * a)
-        # list.sort()
-        # return list
+        for i in range(len(A)):
+            A[i] *= A[i]
+        A.sort()
+        return A
 
-        # second solution
-        length = len(A)
-        res = [0] * length
-        left, right = 0, length - 1
-        for i in range(length - 1, -1, -1):
-            lft = A[left] ** 2
-            rgt = A[right] ** 2
-            if (lft < rgt):
-                res[i] = rgt
-                right -= 1
-            else:
-                res[i] = lft
-                left += 1
-        return res
+    # # second solution
+    # def sortedSquares(self, A: List[int]) -> List[int]:
+    #     length = len(A)
+    #     res = [0] * length
+    #     left, right = 0, length - 1
+    #     for i in range(length - 1, -1, -1):
+    #         lft = A[left] ** 2
+    #         rgt = A[right] ** 2
+    #         if (lft < rgt):
+    #             res[i] = rgt
+    #             right -= 1
+    #         else:
+    #             res[i] = lft
+    #             left += 1
+    #     return res
