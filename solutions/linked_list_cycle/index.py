d3b54f3c096661518133eb0a506cd5504397c4fa
Naoki Mita <naoki.mita18@gmail.com>
2020-02-26 20:14:08 -0800

    Add 141. Linked List Cycle

---
+++ solutions/linked_list_cycle/index.py
@@ -0,0 +1,77 @@
+# 141. Linked List Cycle
+# https://leetcode.com/problems/linked-list-cycle/
+
+from typing import List, Set
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'<{self.val}, {self.left}, {self.right}>'
+
+
+def createTreeNode(list):
+
+    from collections import deque
+
+    data = list
+    n = iter(data)
+    tree = TreeNode(next(n))
+    fringe = deque([tree])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.left = TreeNode(next(n))
+            fringe.append(head.left)
+            head.right = TreeNode(next(n))
+            fringe.append(head.right)
+        except StopIteration:
+            break
+
+    return tree
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def createListNode(list):
+    from collections import deque
+    data = list
+    n = iter(data)
+    node = ListNode(next(n))
+    fringe = deque([node])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.next = ListNode(next(n))
+            fringe.append(head.next)
+        except StopIteration:
+            break
+
+    return node
+
+
+def getValFromListNode(node: ListNode):
+    res: List[int] = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    return res
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        seen: Set[ListNode] = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
---
+++ solutions/linked_list_cycle/test.py
@@ -0,0 +1,19 @@
+import unittest
+import solutions.linked_list_cycle.index as main
+
+
+class Test(unittest.TestCase):
+    def test_hasCycle(self):
+        test_patterns = [
+            ([2, 7, 11, 15], False),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                n = main.createListNode(arg)
+                self.assertEqual(s.hasCycle(n), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
