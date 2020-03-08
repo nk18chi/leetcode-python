b41651a29c4326b519f8318eca349d22d0e5fc66
Naoki Mita <naoki.mita18@gmail.com>
2020-03-07 20:11:45 -0800

    Add weekly 179

---
+++ solutions/_contest/weekly_179/index.py
@@ -0,0 +1,128 @@
+from typing import List, Dict, Set
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'<{self.val}, {self.left}, {self.right}>'
+
+
+def createTreeNode(list):
+
+    from collections import deque
+
+    data = list
+    n = iter(data)
+    tree = TreeNode(next(n))
+    fringe = deque([tree])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.left = TreeNode(next(n))
+            fringe.append(head.left)
+            head.right = TreeNode(next(n))
+            fringe.append(head.right)
+        except StopIteration:
+            break
+
+    return tree
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def createListNode(list):
+    from collections import deque
+    data = list
+    n = iter(data)
+    node = ListNode(next(n))
+    fringe = deque([node])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.next = ListNode(next(n))
+            fringe.append(head.next)
+        except StopIteration:
+            break
+
+    return node
+
+
+def getValFromListNode(node: ListNode):
+    res: List[int] = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    return res
+
+
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        res: str = ""
+        for _ in range(n):
+            res += "a"
+        if n % 2 == 0:
+            res = res[:-1] + "b"
+        return res
+
+    # def numTimesAllBlue(self, light: List[int]) -> int:
+    #     checked: List[int] = [0] * (len(light) + 1)
+    #     checked[0] = 1
+    #     maxVal: int = 0
+    #     count: int = 0
+    #     for n in light:
+    #         checked[n] = 1
+    #         if maxVal == n - 1:
+    #             maxVal = n
+    #             while maxVal < len(light) and checked[maxVal + 1]:
+    #                 maxVal += 1
+    #             if sum(checked) == maxVal + 1:
+    #                 count += 1
+    #     return count
+
+    def numTimesAllBlue(self, light: List[int]) -> int:
+        right = res = 0
+        for i, a in enumerate(light, 1):
+            right = max(right, a)
+            res += right == i
+        return res
+
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        assitance: Dict[int, List[int]] = {}
+        interval: Dict[int, int] = {}
+        for i in range(len(manager)):
+            interval[i] = informTime[i]
+            if manager[i] == -1:
+                continue
+            newAssitance: List[int] = assitance.get(manager[i], [])
+            newAssitance.append(i)
+            assitance[manager[i]] = newAssitance
+
+        def helper(self, i, time) -> int:
+            if i not in assitance:
+                return time
+            time += interval[i]
+            timeList: List[int] = []
+            for a in assitance[i]:
+                timeList.append(helper(self, a, time))
+            return max(timeList)
+
+        return helper(self, headID, 0)
+
+    # def frogPosition(self, n: int, edges: List[List[int]], t: int, target: int) -> float:
+    #     def dfs(self, node: int, probability: List[int], rest: int):
+    #         if rest == 0:
+    #             if node == target:
+    #                 res: int = 0
+    #                 for p in probability:
+    #                     res *= 1 / p
+    #                 return res
+    #             else:
+    #                 return 0
---
+++ solutions/_contest/weekly_179/test.py
@@ -0,0 +1,53 @@
+import unittest
+import solutions._contest.weekly_179.index as main
+
+
+class Test(unittest.TestCase):
+    def test_generateTheString(self):
+        test_patterns = [
+            (4, "aaab"),
+            (5, "aaaaa")
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                # tree = main.createTreeNode([5, 5, 5, 1, 1, 5])
+                # n = main.createListNode(arg)
+                # self.assertEqual(main.getValFromListNode(s.deleteDuplicates(n)), expected)
+                self.assertEqual(s.generateTheString(arg), expected)
+
+    def test_numTimesAllBlue(self):
+        test_patterns = [
+            ([2, 1, 3, 5, 4], 3),
+            ([1, 2, 3, 4, 5, 6], 6),
+            ([4, 1, 2, 3], 1),
+            ([3, 2, 4, 1, 5], 2),
+            ([2, 1, 4, 3, 6, 5], 3),
+            ([1, 2, 3, 4, 5, 6], 6),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                # tree = main.createTreeNode([5, 5, 5, 1, 1, 5])
+                # n = main.createListNode(arg)
+                # self.assertEqual(main.getValFromListNode(s.deleteDuplicates(n)), expected)
+                self.assertEqual(s.numTimesAllBlue(arg), expected)
+
+    def test_numOfMinutes(self):
+        test_patterns = [
+            (15, 0, [-1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6], [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], 3),
+            (6, 2, [2, 2, -1, 2, 2, 2], [0, 0, 1, 0, 0, 0], 1),
+            (7, 6, [1, 2, 3, 4, 5, 6, -1], [0, 6, 5, 4, 3, 2, 1], 21),
+            (4, 2, [3, 3, -1, 2], [0, 0, 162, 914], 1076),
+        ]
+
+        for i, (arg1, arg2, arg3, arg4, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.numOfMinutes(arg1, arg2, arg3, arg4), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
