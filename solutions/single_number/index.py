c61c4e613620f725cca0acf207dd9c970f0b2b53
Naoki Mita <naoki.mita18@gmail.com>
2020-04-01 11:10:53 -0700

    Refactor 136. Single Number

--- solutions/single_number/index.py
+++ solutions/single_number/index.py
@@ -5,5 +5,11 @@ from typing import List
 
 
 class Solution:
+    # def singleNumber(self, nums: List[int]) -> int:
+    #     return sum(set(nums)) * 2 - sum(nums)
+
     def singleNumber(self, nums: List[int]) -> int:
-        return sum(set(nums)) * 2 - sum(nums)
+        res: int = 0
+        for n in nums:
+            res ^= n
+        return res
