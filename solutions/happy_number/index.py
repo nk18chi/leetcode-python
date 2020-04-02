1933fc141183756d9c2ca8f650a7afb183da6987
Naoki Mita <naoki.mita18@gmail.com>
2020-04-02 12:47:11 -0700

    Refactor 202. Happy Number

--- solutions/happy_number/index.py
+++ solutions/happy_number/index.py
@@ -7,9 +7,7 @@ from typing import Set
 class Solution:
     def isHappy(self, n: int) -> bool:
         check: Set[int] = set()
-        while n != 1:
-            if n in check:
-                return False
+        while n not in check:
             check.add(n)
             nxt: int = 0
             while n > 0:
@@ -17,4 +15,4 @@ class Solution:
                 n = n // 10
             n = nxt
 
-        return True
+        return n == 1
