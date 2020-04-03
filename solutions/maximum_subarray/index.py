f0fe4c422dc2f1cef442fc6d57b8b4051b00efb5
Naoki Mita <naoki.mita18@gmail.com>
2020-04-03 12:13:13 -0700

    Refactor 53. Maximum Subarray

--- solutions/maximum_subarray/index.py
+++ solutions/maximum_subarray/index.py
@@ -2,49 +2,12 @@
 # https://leetcode.com/problems/maximum-subarray/
 
 from typing import List
-import unittest
-
-
-def main():
-    unittest.main()
-
-
-class Test(unittest.TestCase):
-
-    def test_maxSubArray(self):
-        test_patterns = [
-            ([2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, -2, -2, 1, -3, 4, -1, 2, 1, -5, 4], 18),
-            ([-2, -2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-            ([-1, 2, -1, 1, 1, -2, 1, -3, 4, -1, 2, 1, -5, 4], 3),
-            ([-2, -2, -1, -3, 0], 0),
-            ([-2, -2, -1, -3, -1], -1)
-        ]
-
-        for i, (arg, expected) in enumerate(test_patterns):
-            with self.subTest(test=i):
-                s = Solution()
-                self.assertEqual(s.maxSubArray(arg), expected)
 
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        max_int = max(nums)
-        if max_int <= 0:
-            return max_int
-
-        sum = 0
-        for n in nums:
-            if n + sum <= 0:
-                sum = 0
-                continue
-
-            sum += n
-            if max_int < sum:
-                max_int = sum
-
-        return max_int
-
-
-if __name__ == '__main__':
-    main()
+        arr: List[int] = [nums[0], nums[0]]
+        for n in nums[1:]:
+            arr[1] = max(n, n + arr[1])
+            arr[0] = max(arr[0], arr[1])
+        return arr[0]
---
+++ solutions/maximum_subarray/test.py
@@ -0,0 +1,22 @@
+import unittest
+import solutions.maximum_subarray.index as main
+
+
+class Test(unittest.TestCase):
+    def test_maxSubArray(self):
+        test_patterns = [
+            ([2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, 2, -1, -2, -2, 1, -3, 4, -1, 2, 1, -5, 4], 18),
+            ([-2, -2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+            ([-1, 2, -1, 1, 1, -2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+            ([-2, -2, -1, -3, 0], 0),
+            ([-2, -2, -1, -3, -1], -1)
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.maxSubArray(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
