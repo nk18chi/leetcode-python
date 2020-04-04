84962fe3399f27aef4cfc232353df88d366176c1
Naoki Mita <naoki.mita18@gmail.com>
2020-04-04 13:11:30 -0700

    Add guard statement on 283

--- solutions/move_zeroes/index.py
+++ solutions/move_zeroes/index.py
@@ -8,6 +8,7 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         pointer: int = 0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[pointer], nums[i] = nums[i], nums[pointer]
-                pointer += 1
+            if nums[i] == 0:
+                continue
+            nums[pointer], nums[i] = nums[i], nums[pointer]
+            pointer += 1
