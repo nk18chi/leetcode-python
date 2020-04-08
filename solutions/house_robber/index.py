222d927425d6599cd8291f93108f6eccb5161244
Naoki Mita <naoki.mita18@gmail.com>
2020-04-08 10:54:16 -0700

    Refactor 198. House Robber

--- solutions/house_robber/index.py
+++ solutions/house_robber/index.py
@@ -5,24 +5,10 @@ from typing import List
 
 
 class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
     def rob(self, nums: List[int]) -> int:
         dp = [0, 0]
         for n in nums:
             dp[0], dp[1] = dp[1], max(dp[1], dp[0] + n)
         return dp[1]
-
-    # # runtime error
-    # def rob(self, nums: List[int]) -> int:
-    #     if len(nums) == 0:
-    #         return 0
-    #     if len(nums) == 1:
-    #         return nums[0]
-    #     if len(nums) == 2:
-    #         return max(nums[0], nums[1])
-    #     if len(nums) == 3:
-    #         return max(nums[0] + nums[2], nums[1])
-
-    #     f = nums[0] + self.rob(nums[2:])
-    #     s = nums[1] + self.rob(nums[3:])
-
-    #     return max(f, s)
