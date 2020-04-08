da04bbc015327ced598ec19500c3a42b78fb4eec
Naoki Mita <naoki.mita18@gmail.com>
2020-04-08 10:34:38 -0700

    Divide into Class.py for ListNode, TreeNode

---
+++ solutions/_class/tree_node.py
@@ -0,0 +1,29 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f'<{self.val}, {self.left}, {self.right}>'
+
+
+def createTreeNode(list):
+
+    from collections import deque
+
+    data = list
+    n = iter(data)
+    tree = TreeNode(next(n))
+    fringe = deque([tree])
+    while True:
+        head = fringe.popleft()
+        try:
+            head.left = TreeNode(next(n))
+            fringe.append(head.left)
+            head.right = TreeNode(next(n))
+            fringe.append(head.right)
+        except StopIteration:
+            break
+
+    return tree
