b359d6aea2cde16f7b2da16c65a6de507169886f
Naoki Mita <naoki.mita18@gmail.com>
2020-04-08 10:33:52 -0700

    Add 876. Middle of the Linked List

---
+++ solutions/middle_of_the_linked_list/index.py
@@ -0,0 +1,32 @@
+# 876. Middle of the Linked List
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+from solutions._class.list_node import ListNode
+
+
+class Solution:
+    # # loop twice
+    # # Time complexity: O(n + n/2) -> O(n)
+    # # Space complexity: O(n)
+    # def middleNode(self, head: ListNode) -> ListNode:
+    #     length: int = 0
+    #     copy: ListNode = head
+    #     while copy:
+    #         length += 1
+    #         copy = copy.next
+    #     mid: int = length // 2
+    #     while mid > 0:
+    #         head = head.next
+    #         mid -= 1
+    #     return head
+
+    # two pointer
+    # Time complexity: O(n/2) -> O(n)
+    # Space complexity: O(1)
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow: ListNode = head
+        fast: ListNode = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
---
+++ solutions/middle_of_the_linked_list/test.py
@@ -0,0 +1,22 @@
+import unittest
+import solutions.middle_of_the_linked_list.index as main
+from solutions._class.list_node import ListNode, createListNode, getValFromListNode
+
+
+class Test(unittest.TestCase):
+    def test_middleNode(self):
+        test_patterns = [
+            ([1, 2, 3, 4, 5], [3, 4, 5]),
+            ([1, 2, 3, 4, 5, 6], [4, 5, 6])
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                ln = createListNode(arg)
+                ans: ListNode = getValFromListNode(s.middleNode(ln))
+                self.assertEqual(ans, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
