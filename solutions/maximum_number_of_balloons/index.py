8526c74dcbc20cfe73be30c21fc3086df0ba1b2d
Naoki Mita <naoki.mita18@gmail.com>
2020-04-10 11:51:15 -0700

    Add 1189. Maximum Number of Balloons

---
+++ solutions/maximum_number_of_balloons/index.py
@@ -0,0 +1,24 @@
+# 1189. Maximum Number of Balloons
+# https://leetcode.com/problems/maximum-number-of-balloons/
+
+from typing import Dict
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def maxNumberOfBalloons(self, text: str) -> int:
+        charMap: Dict[str, int] = {
+            "b": 0,
+            "a": 0,
+            "l": 0,
+            "o": 0,
+            "n": 0
+        }
+        for t in text:
+            if t not in charMap:
+                continue
+            charMap[t] += 1
+        charMap["l"] //= 2
+        charMap["o"] //= 2
+        return min(charMap.values())
---
+++ solutions/maximum_number_of_balloons/test.py
@@ -0,0 +1,18 @@
+import unittest
+import solutions.maximum_number_of_balloons.index as main
+
+
+class Test(unittest.TestCase):
+    def test_maxNumberOfBalloons(self):
+        test_patterns = [
+            ("loonbalxballpoon", 2),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.maxNumberOfBalloons(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
