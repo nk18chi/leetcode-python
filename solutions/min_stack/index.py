7d37ba9cebfe89d2b74e49a87f95e25fda89352a
Naoki Mita <naoki.mita18@gmail.com>
2020-04-10 11:29:26 -0700

    Add 155. Min Stack

---
+++ solutions/min_stack/index.py
@@ -0,0 +1,25 @@
+# 155. Min Stack
+# https://leetcode.com/problems/min-stack/
+
+from typing import List, Tuple, Optional
+
+
+class Solution:
+    class MinStack:
+        def __init__(self):
+            self.stack: List[Tuple[int, int]] = []
+
+        def push(self, x: int) -> None:
+            currentMin: Optional[int] = self.getMin()
+            if currentMin is None or currentMin > x:
+                currentMin = x
+            self.stack.append((x, currentMin))
+
+        def pop(self) -> None:
+            self.stack.pop()
+
+        def top(self) -> Optional[int]:
+            return self.stack[-1][0] if len(self.stack) > 0 else None
+
+        def getMin(self) -> Optional[int]:
+            return self.stack[-1][1] if len(self.stack) > 0 else None
---
+++ solutions/min_stack/test.py
@@ -0,0 +1,24 @@
+import unittest
+import solutions.min_stack.index as main
+
+
+class Test(unittest.TestCase):
+    def test_functionName(self):
+        test_patterns = [
+            (-3, 0, -2),
+        ]
+
+        for i, (arg1, arg2, arg3) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                minStack = main.Solution().MinStack()
+                minStack.push(-2)
+                minStack.push(0)
+                minStack.push(-3)
+                self.assertEqual(minStack.getMin(), arg1)
+                minStack.pop()
+                self.assertEqual(minStack.top(), arg2)
+                self.assertEqual(minStack.getMin(), arg3)
+
+
+if __name__ == '__main__':
+    unittest.main()
