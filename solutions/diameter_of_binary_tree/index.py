0112017b9fb6e0bfe45f0d2188bc599f7775a7ec
Naoki Mita <naoki.mita18@gmail.com>
2020-04-11 14:44:17 -0700

    Add 543. Diameter of Binary Tree

---
+++ solutions/diameter_of_binary_tree/index.py
@@ -0,0 +1,20 @@
+# 543. Diameter of Binary Tree
+# https://leetcode.com/problems/diameter-of-binary-tree/
+
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.maxPath: int = 0
+
+        def dfs(root: TreeNode):
+            if root is None:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.maxPath = max(self.maxPath, left + right)
+            return max(left, right) + 1
+
+        dfs(root)
+        return self.maxPath
---
+++ solutions/diameter_of_binary_tree/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.diameter_of_binary_tree.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_diameterOfBinaryTree(self):
+        test_patterns = [
+            ([1, 2, 3, 4, 5, 6, 7], 4),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree: TreeNode = createTreeNode(arg)
+                self.assertEqual(s.diameterOfBinaryTree(tree), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
