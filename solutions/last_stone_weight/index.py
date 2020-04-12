536873d4e238d84f84764d30807bad0bbebeb972
Naoki Mita <naoki.mita18@gmail.com>
2020-04-12 14:02:05 -0700

    Refactor 1046. Last Stone Weight

--- solutions/last_stone_weight/index.py
+++ solutions/last_stone_weight/index.py
@@ -5,39 +5,35 @@ from typing import List
 
 
 class Solution:
-    # first solution
+    # sort solution
+    # Time complexity: O(n^2logn)
+    # Space complexity: O(1)
     def lastStoneWeight(self, stones: List[int]) -> int:
         while len(stones) > 1:
-            stones.sort(reverse=True)
-            if stones[0] == stones[1]:
-                del stones[1]
-                del stones[0]
-            else:
-                stones[0] = stones[0] - stones[1]
-                del stones[1]
+            stones.sort()
+            max1: int = stones.pop()
+            max2: int = stones.pop()
+            if max1 > max2:
+                stones.append(max1 - max2)
         return stones[0] if len(stones) == 1 else 0
 
-        # # second solution
-        # while len(stones) > 1:
-        #     _max = max(stones)
-        #     stones.remove(_max)
-        #     _max_second = max(stones)
-        #     stones.remove(_max_second)
-        #     if _max != _max_second:
-        #         stones.append(_max - _max_second)
-        # return stones[0] if len(stones) == 1 else 0
+    # # priority queue solution1 by @lee215
+    # # Time complexity: O(nlogn)
+    # # Space complexity: O(n)
+    # def lastStoneWeight(self, stones: List[int]) -> int:
+    #     import heapq
+    #     h = [-x for x in stones]
+    #     heapq.heapify(h)
+    #     while len(h) > 1 and h[0] != 0:
+    #         heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
+    #     return -h[0]
 
-        # # Priority Queue1
-        # import heapq
-        # h = [-x for x in stones]
-        # heapq.heapify(h)
-        # while len(h) > 1 and h[0] != 0:
-        #     heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
-        # return -h[0]
-
-        # # Priority Queue2
-        # import bisect
-        # stones.sort()
-        # while len(stones) > 1:
-        #     bisect.insort(stones, stones.pop() - stones.pop())
-        # return stones[0]
+    # # priority queue solution2 by @lee215
+    # # Time complexiy: O(n^2)
+    # # Space complexiy: O(1)
+    # def lastStoneWeight(self, stones: List[int]) -> int:
+    #     import bisect
+    #     stones.sort()
+    #     while len(stones) > 1:
+    #         bisect.insort(stones, stones.pop() - stones.pop())
+    #     return stones[0]
