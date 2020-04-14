a4982ad3f506c29691caf502bc7b0f8ba56805a7
Naoki Mita <naoki.mita18@gmail.com>
2020-04-14 12:04:36 -0700

    Refactor 746. Min Cost Climbing Stairs

--- solutions/min_cost_climbing_stairs/test.py
+++ solutions/min_cost_climbing_stairs/test.py
@@ -6,9 +6,9 @@ import solutions.min_cost_climbing_stairs.index as main
 class Test(unittest.TestCase):
     def test_minCostClimbingStairs(self):
         test_patterns = [
+            ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6),
             ([1, 0, 2, 2], 2),
             ([10, 15, 20], 15),
-            ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6)
         ]
 
         for i, (arg, expected) in enumerate(test_patterns):
