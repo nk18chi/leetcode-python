c392c123814ce437e97f25b4e2c92be51a11e53a
Naoki Mita <naoki.mita18@gmail.com>
2020-04-18 22:12:55 -0700

    Add weekly 185

---
+++ solutions/_contest/weekly_185/index.py
@@ -0,0 +1,72 @@
+from typing import List, Dict, Set, Tuple
+
+
+class Solution:
+    def reformat(self, s: str) -> str:
+        import itertools
+        string: str = ""
+        i: str = ""
+        for c in s:
+            if str.isdigit(c):
+                i += c
+            else:
+                string += c
+        if len(i) > len(string):
+            string, i = i, string
+        if len(string) - len(i) > 1:
+            return ""
+        res: str = ""
+        for f, s in itertools.zip_longest(string, i):
+            res += f
+            if s:
+                res += s
+        return res
+
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        orders.sort(key=lambda x: x[2])
+        header: List[str] = []
+        indexMap: Dict[int, List[int]] = {}
+        for o in orders:
+            if len(header) == 0 or header[-1] != o[2]:
+                header.append(o[2])
+                for k, v in indexMap.items():
+                    indexMap[k] = v + [0]
+            if int(o[1]) in indexMap:
+                indexMap[int(o[1])][-1] += 1
+            else:
+                indexMap[int(o[1])] = [0] * len(header)
+                indexMap[int(o[1])][-1] += 1
+        sortedMap = sorted(indexMap.items())
+        res: List[List[str]] = []
+        res.append(["Table"] + header)
+        for k, v in sortedMap:
+            res.append([str(k)] + [str(x) for x in v])
+        return res
+
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        char: Dict[str, int] = {"c": 0, "r": 0, "o": 0, "a": 0, "k": 0}
+        prev: Dict[str, str] = {"r": "c", "o": "r", "a": "o", "k": "a"}
+        res: int = 0
+        for c in croakOfFrogs:
+            char[c] += 1
+            if c != "c" and char[c] > char[prev[c]]:
+                return - 1
+            if c != "k":
+                res = max(res, char[c] - char["k"])
+        return res if char["c"] == char["k"] else -1
+
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        mod = 10**9 + 7
+
+        def dfs(i, j, k):
+            if k < 0:
+                return 0
+            if i > n - 1:
+                return k == 0
+            cnt = 0
+            for nj in range(1, j + 1):
+                cnt = (cnt + dfs(i + 1, j, k)) % mod
+            for nj in range(j + 1, m + 1):
+                cnt = (cnt + dfs(i + 1, nj, k - 1)) % mod
+            return cnt
+        return dfs(0, 0, k)
---
+++ solutions/_contest/weekly_185/test.py
@@ -0,0 +1,52 @@
+import unittest
+import solutions._contest.weekly_185.index as main
+
+
+class Test(unittest.TestCase):
+    def test_reformat(self):
+        test_patterns = [
+            ("covid2019", "c2o0v1i9d"),
+            ("a0b1c2", "a0b1c2"),
+            ("leetcode", ""),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.reformat(arg), expected)
+
+    def test_displayTable(self):
+        test_patterns = [([["David", "3", "Ceviche"], ["Corina", "10", "Beef Burrito"], ["David", "3", "Fried Chicken"], ["Carla", "5", "Water"], ["Carla", "5", "Ceviche"], [
+                           "Rous", "3", "Ceviche"]], [["Table", "Beef Burrito", "Ceviche", "Fried Chicken", "Water"], ["3", "0", "2", "1", "0"], ["5", "0", "1", "0", "1"], ["10", "1", "0", "0", "0"]]), ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.displayTable(arg), expected)
+
+    def test_minNumberOfFrogs(self):
+        test_patterns = [
+            ("croakcroa", -1),
+            ("crcoakroak", 2),
+            ("croakcroak", 1),
+            ("croakcrook", -1),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.minNumberOfFrogs(arg), expected)
+
+    def test_numOfArrays(self):
+        test_patterns = [
+            (2, 3, 1, 6),
+        ]
+
+        for i, (arg1, arg2, arg3, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.numOfArrays(arg1, arg2, arg3), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
