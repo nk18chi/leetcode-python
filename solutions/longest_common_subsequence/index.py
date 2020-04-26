e6664c93d9b6292afbbf2d266a4d701043c8cf8b
Naoki Mita <naoki.mita18@gmail.com>
2020-04-26 15:23:29 -0700

    Add 1143. Longest Common Subsequence

---
+++ solutions/longest_common_subsequence/index.py
@@ -0,0 +1,16 @@
+# 1143. Longest Common Subsequence
+# https://leetcode.com/problems/longest-common-subsequence/
+
+from typing import List
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp: List[List[int]] = [[0 for _ in range(len(text1) + 1)] for _ in range(len(text2) + 1)]
+        for i in range(1, len(text2) + 1):
+            for j in range(1, len(text1) + 1):
+                if text2[i - 1] == text1[j - 1]:
+                    dp[i][j] += dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] += max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
---
+++ solutions/longest_common_subsequence/test.py
@@ -0,0 +1,23 @@
+import unittest
+import solutions.longest_common_subsequence.index as main
+
+
+class Test(unittest.TestCase):
+    def test_longestCommonSubsequence(self):
+        test_patterns = [
+            ("abaa", "ab", 2),
+            ("abcde", "ace", 3),
+            ("bsbininm", "jmjkbkjkv", 1),
+            ("abc", "abc", 3),
+            ("abc", "def", 0),
+            ("bl", "yby", 1),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.longestCommonSubsequence(arg1, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
