1e0806ab8f89fc69c3ebb45e945326fea1f0511c
Naoki Mita <naoki.mita18@gmail.com>
2020-04-30 10:40:03 -0700

    Add isValidSequence

---
+++ solutions/isValidSequence/index.py
@@ -0,0 +1,16 @@
+# Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+# https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/532/week-5/3315/
+
+from typing import List
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
+        def dfs(root: TreeNode, index: int) -> bool:
+            if not root or len(arr) <= index or root.val != arr[index]:
+                return False
+            elif len(arr) == index + 1 and not root.left and not root.right:
+                return True
+            return dfs(root.left, index + 1) or dfs(root.right, index + 1)
+        return dfs(root, 0)
---
+++ solutions/isValidSequence/test.py
@@ -0,0 +1,21 @@
+import unittest
+import solutions.isValidSequence.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_isValidSequence(self):
+        test_patterns = [
+            ([0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1], True),
+            ([0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0], False),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree: TreeNode = createTreeNode(arg1)
+                self.assertEqual(s.isValidSequence(tree, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
