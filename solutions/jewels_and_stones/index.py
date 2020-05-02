672075268d2db85b7e50be35e5c9da910e9fc85e
Naoki Mita <naoki.mita18@gmail.com>
2020-05-02 11:55:04 -0700

    Refactor 771. Jewels and Stones

--- solutions/jewels_and_stones/index.py
+++ solutions/jewels_and_stones/index.py
@@ -1,18 +1,12 @@
 # 771. Jewels and Stones
 # https://leetcode.com/problems/jewels-and-stones/
 
-# from typing import List
+from typing import Set
 
 
 class Solution:
+    # Time complexity: (J + S)
+    # Space complexity: (J)
     def numJewelsInStones(self, J: str, S: str) -> int:
-        dict = {}
-        for s in S:
-            dict[s] = dict[s] + 1 if s in dict else 1
-
-        total_count = 0
-        for j in J:
-            if j in dict:
-                total_count += dict[j]
-
-        return total_count
+        jewels: Set[str] = set(J)
+        return sum(s in jewels for s in S)
