876aba4de8419ebd94f91dc85910fa6d1ad7e81d
Naoki Mita <naoki.mita18@gmail.com>
2020-05-04 12:08:22 -0700

    Refactor 476. Number Complement

--- solutions/number_complement/index.py
+++ solutions/number_complement/index.py
@@ -1,54 +1,11 @@
 # 476. Number Complement
 # https://leetcode.com/problems/number-complement/
 
-# from typing import List
+import math
 
 
 class Solution:
+    # Time complexity: O(logn)
+    # Space complexity: O(1)
     def findComplement(self, num: int) -> int:
-        # # first solution
-        # bit_num = format(num, 'b')
-        # res = ""
-        # for n in bit_num:
-        #     res += "0" if n == '1' else "1"
-
-        # return int(res, 2)
-
-        # second solution
-        bit_num = format(num, 'b')
-        res = ""
-        for n in bit_num:
-            res += str(int(n) ^ 1)
-
-        return int(res, 2)
-
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-    def __str__(self):
-        return f'<{self.val}, {self.left}, {self.right}>'
-
-
-def createTreeNode(list):
-
-    from collections import deque
-
-    data = list
-    n = iter(data)
-    tree = TreeNode(next(n))
-    fringe = deque([tree])
-    while True:
-        head = fringe.popleft()
-        try:
-            head.left = TreeNode(next(n))
-            fringe.append(head.left)
-            head.right = TreeNode(next(n))
-            fringe.append(head.right)
-        except StopIteration:
-            break
-
-    return tree
+        return pow(2, math.floor(math.log(num, 2)) + 1) - 1 ^ num
