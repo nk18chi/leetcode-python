ee382fdf44c49598ae474aecbda029aefab302aa
Naoki Mita <naoki.mita18@gmail.com>
2020-05-04 14:35:17 -0700

    Refactor weekly 187

--- solutions/_contest/weekly_187/index.py
+++ solutions/_contest/weekly_187/index.py
@@ -1,4 +1,5 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple
+import heapq
 
 
 class Solution:
@@ -26,17 +27,34 @@ class Solution:
                 count -= 1
         return True
 
-    # TLE
+    # # TLE
+    # def longestSubarray(self, nums: List[int], limit: int) -> int:
+    #     res: int = 1
+    #     for i in range(len(nums)):
+    #         _max = nums[i]
+    #         _min = nums[i]
+    #         for j in range(i + 1, len(nums)):
+    #             _max = max(_max, nums[j])
+    #             _min = min(_min, nums[j])
+    #             if abs(_max - _min) > limit:
+    #                 break
+    #             else:
+    #                 res = max(res, j - i + 1)
+    #     return res
+
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        res: int = 1
+        maxq: List[Tuple[int, int]] = []
+        minq: List[Tuple[int, int]] = []
+        res: int = 0
+        pointer: int = 0
         for i in range(len(nums)):
-            _max = nums[i]
-            _min = nums[i]
-            for j in range(i + 1, len(nums)):
-                _max = max(_max, nums[j])
-                _min = min(_min, nums[j])
-                if abs(_max - _min) > limit:
-                    break
-                else:
-                    res = max(res, j - i + 1)
+            heapq.heappush(maxq, (-nums[i], i))
+            heapq.heappush(minq, (nums[i], i))
+            while - maxq[0][0] - minq[0][0] > limit:
+                pointer = min(maxq[0][1], minq[0][1]) + 1
+                while maxq[0][1] < pointer:
+                    heapq.heappop(maxq)
+                while minq[0][1] < pointer:
+                    heapq.heappop(minq)
+            res = max(res, i - pointer + 1)
         return res
