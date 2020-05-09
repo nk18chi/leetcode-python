1a306920039cb98d37a6b78bad594386a4bc2bda
Naoki Mita <naoki.mita18@gmail.com>
2020-05-09 12:22:00 -0700

    Refactor 367. Valid Perfect Square

--- solutions/valid_perfect_squar/index.py
+++ solutions/valid_perfect_squar/index.py
@@ -5,81 +5,18 @@
 
 
 class Solution:
-    # third solution
+    # Binary Search Solution
+    # Time complexity: O(logn)
+    # Space complexity: O(1)
     def isPerfectSquare(self, num: int) -> bool:
-        start = 0
-        end = num
-        while (True):
-            mid = (end + start) // 2
-            if num == mid * mid:
+        left: int = 0
+        right: int = num
+        while left <= right:
+            mid: int = (left + right) // 2
+            if mid ** 2 == num:
                 return True
-            elif start > end:
-                return False
-            elif num < mid * mid:
-                end = mid - 1
+            elif mid ** 2 > num:
+                right = mid - 1
             else:
-                start = mid + 1
-
-    # # fourth solution
-    # def isPerfectSquare(self, num: int) -> bool:
-    #     i = 1
-    #     while num > 0:
-    #         num -= i
-    #         i += 2
-    #     return num == 0
-
-    # # second solution
-    # def isPerfectSquare(self, num: int) -> bool:
-    #     return self.isSqrtByBinarySearch(num, 0, num)
-
-    # def isSqrtByBinarySearch(self, num, start, end):
-    #     mid = (end + start) // 2
-    #     if start > end:
-    #         return False
-    #     elif num == mid * mid:
-    #         return True
-    #     elif num < mid * mid:
-    #         return self.isSqrtByBinarySearch(num, start, mid - 1)
-    #     else:
-    #         return self.isSqrtByBinarySearch(num, mid + 1, end)
-
-    # # first solution
-    # def isPerfectSquare(self, num: int) -> bool:
-    #     index = 2
-    #     while (num / 2 >= index):
-    #         if index == num / index:
-    #             return True
-    #         index += 1
-
-    #     return False
-
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-    def __str__(self):
-        return f'<{self.val}, {self.left}, {self.right}>'
-
-
-def createTreeNode(list):
-
-    from collections import deque
-
-    data = list
-    n = iter(data)
-    tree = TreeNode(next(n))
-    fringe = deque([tree])
-    while True:
-        head = fringe.popleft()
-        try:
-            head.left = TreeNode(next(n))
-            fringe.append(head.left)
-            head.right = TreeNode(next(n))
-            fringe.append(head.right)
-        except StopIteration:
-            break
-
-    return tree
+                left = mid + 1
+        return False
--- solutions/valid_perfect_squar/test.py
+++ solutions/valid_perfect_squar/test.py
@@ -6,16 +6,15 @@ import solutions.valid_perfect_squar.index as main
 class Test(unittest.TestCase):
     def test_isPerfectSquare(self):
         test_patterns = [
+            (14, False),
+            (16, True),
             (1, True),
             (5, False),
-            (16, True),
-            (14, False)
         ]
 
         for i, (arg, expected) in enumerate(test_patterns):
             with self.subTest(test=i):
                 s = main.Solution()
-                # tree = f.createTreeNode([5, 5, 5, 1, 1, 5])
                 self.assertEqual(s.isPerfectSquare(arg), expected)
 
 
