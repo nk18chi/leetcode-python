b1a95998a8587a7f922a4d35a3cab2c1e527fe0a
Naoki Mita <naoki.mita18@gmail.com>
2020-05-11 17:23:37 -0700

    Refactor 733. Flood Fill

--- solutions/flood_fill/index.py
+++ solutions/flood_fill/index.py
@@ -1,36 +1,26 @@
 # 733. Flood Fill
-# # https://leetcode.com/problems/flood-fill/
+# https://leetcode.com/problems/flood-fill/
 
 from typing import List
 
 
 class Solution:
-    def floodFill(self,
-                  image: List[List[int]],
-                  sr: int,
-                  sc: int,
-                  newColor: int) -> List[List[int]]:
-
-        self.max_heigh = len(image)
-        self.max_width = len(image[0])
-        self.image = image
-        self.newColor = newColor
-        self.prevColor = self.image[sr][sc]
-        self.fillSquare(sr, sc)
-
-        return self.image
-
-    def fillSquare(self, r, c):
-        if r < 0 or r >= self.max_heigh:
-            return
-        elif c < 0 or c >= self.max_width:
-            return
-        elif self.image[r][c] != self.prevColor:
-            return
-        else:
-            self.image[r][c] = -1
-            self.fillSquare(r + 1, c)
-            self.fillSquare(r - 1, c)
-            self.fillSquare(r, c + 1)
-            self.fillSquare(r, c - 1)
-            self.image[r][c] = self.newColor
+    # Time complexity: O(mn)
+    # Space complexity: O(mn)
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        def dfs(row: int, col: int):
+            if row < 0 or row >= len(image):
+                return
+            if col < 0 or col >= len(image[0]):
+                return
+            if image[row][col] != originColor:
+                return
+            image[row][col] = newColor
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+        originColor = image[sr][sc]
+        if originColor != newColor:
+            dfs(sr, sc)
+        return image
