c88cfb717d144551795edc7df2378a68fa3c92b4
Naoki Mita <naoki.mita18@gmail.com>
2020-05-18 16:03:13 -0700

    Add 567. Permutation in String

---
+++ solutions/permutation_in_string/index.py
@@ -0,0 +1,23 @@
+# 567. Permutation in String
+# https://leetcode.com/problems/permutation-in-string/
+
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        target: List[int] = [0] * 26
+        window: List[int] = [0] * 26
+        for i in range(len(s1)):
+            target[ord(s1[i]) - ord('a')] += 1
+        for i, c in enumerate(s2):
+            window[ord(c) - ord('a')] += 1
+            if i >= len(s1):
+                window[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            if window == target:
+                return True
+        return False
---
+++ solutions/permutation_in_string/test.py
@@ -0,0 +1,22 @@
+import unittest
+import solutions.permutation_in_string.index as main
+
+
+class Test(unittest.TestCase):
+    def test_checkInclusion(self):
+        test_patterns = [
+            ("adc", "dcda", True),
+            ("ab", "eidbaooo", True),
+            ("a", "ab", True),
+            ("ab", "eidboaoo", False),
+            ("ab", "ab", True),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.checkInclusion(arg1, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
