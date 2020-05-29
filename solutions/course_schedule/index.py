278c5d16c46079f75853064609e83e3ae4360b99
Naoki Mita <naoki.mita18@gmail.com>
2020-05-29 11:19:35 -0700

    Add 207. Course Schedule

---
+++ solutions/course_schedule/index.py
@@ -0,0 +1,21 @@
+# 207. Course Schedule
+# https://leetcode.com/problems/course-schedule/
+
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph: List[List[int]] = [[] for _ in range(numCourses)]
+        indegree: List[int] = [0] * numCourses
+        for _to, _from in prerequisites:
+            graph[_from].append(_to)
+            indegree[_to] += 1
+        queue: List[int] = [i for i, x in enumerate(indegree) if x == 0]
+        while queue:
+            n: int = queue.pop()
+            for g in graph[n]:
+                indegree[g] -= 1
+                if indegree[g] == 0:
+                    queue.append(g)
+        return sum(indegree) == 0
---
+++ solutions/course_schedule/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.course_schedule.index as main
+
+
+class Test(unittest.TestCase):
+    def test_canFinish(self):
+        test_patterns = [
+            (8, [[1, 0], [2, 6], [1, 7], [6, 4], [7, 0], [0, 5]], True),
+            (2, [[0, 1]], True),
+            (6, [[1, 0], [2, 0], [2, 5], [3, 2], [5, 1], [4, 2]], True),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.canFinish(arg1, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
