34d2d9b9d6c6a742c1991a01d1ea89e03cc7c67e
Naoki Mita <naoki.mita18@gmail.com>
2020-06-08 10:57:07 -0700

    Refactor 231. Power of Two

--- solutions/power_of_two/index.py
+++ solutions/power_of_two/index.py
@@ -3,23 +3,7 @@
 
 
 class Solution:
+    # Time complexity: O(1)
+    # Space complexity: O(1)
     def isPowerOfTwo(self, n: int) -> bool:
-        # first solution
-        if n == 0:
-            return False
-        str = format(n, 'b')
-        for s in str[1:]:
-            if int(s) == 1:
-                return False
-
-        return True
-
-        # # second solution
-        # if n == 0:
-        #     return False
-        # if n == 1:
-        #     return True
-        # i = int(format(n, 'b')[1:])
-        # if i == 0:
-        #     return True
-        # return False
+        return n > 0 and n & (n - 1) == 0
