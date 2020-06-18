ccb62636b1b7873a410b81655d27ae948e8a8f66
Naoki Mita <naoki.mita18@gmail.com>
2020-06-18 16:46:04 -0700

    Add 274. H-Index

---
+++ solutions/h_Index/test.py
@@ -0,0 +1,18 @@
+import unittest
+import solutions.h_Index.index as main
+
+
+class Test(unittest.TestCase):
+    def test_hIndex(self):
+        test_patterns = [
+            ([3, 0, 6, 1, 5], 3),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.hIndex(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
