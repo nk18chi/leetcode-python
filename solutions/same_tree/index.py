72617cd824db233123400dd8df2c042cd75787ea
Naoki Mita <naoki.mita18@gmail.com>
2020-07-13 10:49:50 -0700

    Add 100. Same Tree

---
+++ solutions/same_tree/index.py
@@ -0,0 +1,18 @@
+# 100. Same Tree
+# https://leetcode.com/problems/same-tree/
+
+# from typing import List, Dict
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
---
+++ solutions/same_tree/test.py
@@ -0,0 +1,23 @@
+import unittest
+import solutions.same_tree.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_isSameTree(self):
+        test_patterns = [
+            ([1, 2, 3], [1, 2, 3], True),
+            ([1, 2], [1, None, 2], False),
+            ([1, 2, 1], [1, 1, 2], False),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree1: TreeNode = createTreeNode(arg1)
+                tree2: TreeNode = createTreeNode(arg2)
+                self.assertEqual(s.isSameTree(tree1, tree2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
