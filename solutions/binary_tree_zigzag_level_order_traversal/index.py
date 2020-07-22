510c3e583fe0951c97ebb5c1335ed56b6d3d63df
Naoki Mita <naoki.mita18@gmail.com>
2020-07-22 16:14:14 -0700

    Add 103. Binary Tree Zigzag Level Order Traversal

---
+++ solutions/binary_tree_zigzag_level_order_traversal/index.py
@@ -0,0 +1,33 @@
+# 103. Binary Tree Zigzag Level Order Traversal
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+from typing import List
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        res: List[List[int]] = []
+        queue: List[TreeNode] = [root]
+        isLeftToRight: bool = True
+        while queue:
+            nxt: List[TreeNode] = []
+            level: List[int] = []
+            while queue:
+                node: TreeNode = queue.pop()
+                if node is None:
+                    continue
+                level.append(node.val)
+                if isLeftToRight:
+                    nxt.append(node.left)
+                    nxt.append(node.right)
+                else:
+                    nxt.append(node.right)
+                    nxt.append(node.left)
+            if level:
+                res.append(level)
+            queue = nxt
+            isLeftToRight = not isLeftToRight
+        return res
---
+++ solutions/binary_tree_zigzag_level_order_traversal/test.py
@@ -0,0 +1,21 @@
+import unittest
+import solutions.binary_tree_zigzag_level_order_traversal.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_zigzagLevelOrder(self):
+        test_patterns = [
+            ([3, 9, 20, 10, 2, 15, 7], [[3], [20, 9], [15, 7, 10, 2]]),
+            ([3, 9, 20, None, None, 15, 7], [[3], [20, 9], [15, 7]]),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree: TreeNode = createTreeNode(arg)
+                self.assertEqual(s.zigzagLevelOrder(tree), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
