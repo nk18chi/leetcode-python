39acc8a421744c0c1374f93ae1259637230a00fb
Naoki Mita <naoki.mita18@gmail.com>
2020-07-25 21:11:34 -0700

    Add weekly 199

---
+++ solutions/_contest/weekly_199/index.py
@@ -0,0 +1,59 @@
+from typing import List, Set, Dict, Tuple, Deque
+from collections import deque, defaultdict
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res: List[str] = ["" for _ in range(len(s))]
+        for i, n in enumerate(indices):
+            res[n] = s[i]
+        return "".join(res)
+
+    def minFlips(self, target: str) -> int:
+        intTarget: int = int(target)
+        target = str(intTarget)
+        if target == "0":
+            return 0
+        count: int = 0
+        for i in range(1, len(target)):
+            if target[i - 1] == target[i]:
+                count += 1
+        return len(target) - count
+
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.res: int = 0
+
+        def dfs(root: TreeNode):
+            if root is None:
+                return None
+            left: Deque[int] = dfs(root.left)
+            right: Deque[int] = dfs(root.right)
+            if left is None and right is None:
+                return deque([1])
+            if left is None or right is None:
+                d: Deque[int] = left if right is None else right
+                while d and d[0] + 1 >= distance:
+                    d.popleft()
+                if d:
+                    for i in range(len(d)):
+                        d[i] += 1
+                return d
+            for i in range(len(left)):
+                for j in range(len(right)):
+                    if left[i] + right[j] > distance:
+                        break
+                    self.res += 1
+            nxt: Deque[int] = deque([])
+            while left and right:
+                if left[0] < right[0]:
+                    nxt.append(left.popleft() + 1)
+                else:
+                    nxt.append(right.popleft() + 1)
+            d = left if left else right
+            while d:
+                nxt.append(d.popleft() + 1)
+            return nxt
+
+        dfs(root)
+        return self.res
---
+++ solutions/_contest/weekly_199/test.py
@@ -0,0 +1,41 @@
+import unittest
+import solutions._contest.weekly_199.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_restoreString(self):
+        test_patterns = [
+            ("codeleet", [4, 5, 6, 7, 0, 2, 1, 3], "leetcode"),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.restoreString(arg1, arg2), expected)
+
+    def test_minFlips(self):
+        test_patterns = [
+            ("10111", 3),
+            ("00000", 0),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.minFlips(arg1), expected)
+
+    def test_countPairs(self):
+        test_patterns = [
+            ([1, 2, 3, 4, 5, 6, 7], 3, 2),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree: TreeNode = createTreeNode(arg1)
+                self.assertEqual(s.countPairs(tree, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
