226aec0b3566094ca452632dfceb8bc23f6041e0
Naoki Mita <naoki.mita18@gmail.com>
2020-07-27 11:39:12 -0700

    Add 106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ solutions/construct_binary_tree_from_inorder_and_postorder_traversal/index.py
@@ -0,0 +1,25 @@
+# 106. Construct Binary Tree from Inorder and Postorder Traversal
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+from typing import List, Dict
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        def helper(start: int, end: int) -> TreeNode:
+            if start > end:
+                return None
+            idx: int = postorder.pop()
+            root: TreeNode = TreeNode(idx)
+            divided: int = indexMap[idx]
+            root.right = helper(divided + 1, end)
+            root.left = helper(start, divided - 1)
+            return root
+
+        indexMap: Dict[int, int] = {}
+        for i, n in enumerate(inorder):
+            indexMap[n] = i
+        return helper(0, len(inorder) - 1)
---
+++ solutions/construct_binary_tree_from_inorder_and_postorder_traversal/test.py
@@ -0,0 +1,19 @@
+import unittest
+import solutions.construct_binary_tree_from_inorder_and_postorder_traversal.index as main
+from solutions._class.tree_node import createTreeNode, getTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_buildTree(self):
+        test_patterns = [
+            ([11, 9, 14, 3, 15, 20, 7], [11, 14, 9, 15, 7, 20, 3], [3, 9, 20, 11, 14, 15, 7]),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(getTreeNode(s.buildTree(arg1, arg2)), getTreeNode(createTreeNode(expected)))
+
+
+if __name__ == '__main__':
+    unittest.main()
