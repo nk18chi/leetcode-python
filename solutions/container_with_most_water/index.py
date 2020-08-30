b1bb11d156a52721a7cf938a372ba0a163ef8f3c
Naoki Mita <naoki.mita18@gmail.com>
2020-08-30 16:44:53 -0700

    Add 11. Container With Most Water

---
+++ solutions/container_with_most_water/index.py
@@ -0,0 +1,25 @@
+# 11. Container With Most Water
+# https://leetcode.com/problems/container-with-most-water/
+
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def maxArea(self, height: List[int]) -> int:
+        res: int = 0
+        left: int = 0
+        right: int = len(height) - 1
+        while (left < right):
+            square: int = min(height[left], height[right]) * (right - left)
+            res = max(res, square)
+            if height[left] == height[right]:
+                left += 1
+                right -= 1
+            elif height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return res
---
+++ solutions/container_with_most_water/test.py
@@ -0,0 +1,18 @@
+import unittest
+import solutions.container_with_most_water.index as main
+
+
+class Test(unittest.TestCase):
+    def test_maxArea(self):
+        test_patterns = [
+            ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.maxArea(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
