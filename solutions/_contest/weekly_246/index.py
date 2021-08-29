2d0c32f0f4bcaad966eed7495dac4e7685c29138
Naoki Mita <naoki.mita18@gmail.com>
2021-08-28 19:28:28 -0700

    add: weekly 246

---
+++ solutions/_contest/weekly_246/index.py
@@ -0,0 +1,16 @@
+from typing import List, Set, Dict, Tuple, Deque
+from collections import deque, defaultdict
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        count: int = 0
+        for a in arr:
+            if a % 2 == 0:
+                count = 0
+            else:
+                count += 1
+                if count == 3:
+                    return True
+        return False
---
+++ solutions/_contest/weekly_246/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions._contest.weekly_246.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_threeConsecutiveOdds(self):
+        test_patterns = [
+            ([2, 6, 4, 1], False),
+            ([1, 2, 34, 3, 4, 5, 7, 23, 12], True),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.threeConsecutiveOdds(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
