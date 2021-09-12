6117c0fcd757f71fe1ebe9033c8e961a574e6904
Naoki Mita <naoki.mita18@gmail.com>
2021-09-12 12:08:44 -0700

    add: weekly contest 257

---
+++ solutions/_contest/weekly_257/index.py
@@ -0,0 +1,26 @@
+from typing import List, Set, Dict, Tuple, Deque
+from collections import deque, defaultdict
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        res: int = 0
+        for one in range(len(nums)):
+            for two in range(one + 1, len(nums)):
+                for three in range(two + 1, len(nums)):
+                    for four in range(three + 1, len(nums)):
+                        if nums[one] + nums[two] + nums[three] == nums[four]:
+                            res += 1
+        return res
+
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key=lambda x: (-x[0], x[1]))
+        ans = 0
+        curr_max = 0
+        for _, d in properties:
+            if d < curr_max:
+                ans += 1
+            else:
+                curr_max = d
+        return ans
---
+++ solutions/_contest/weekly_257/test.py
@@ -0,0 +1,34 @@
+import unittest
+import solutions._contest.weekly_257.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_countQuadruplets(self):
+        test_patterns = [
+            ([1, 2, 3, 6], 1),
+            ([3, 3, 6, 4, 5], 0),
+            ([1, 1, 1, 3, 5], 4),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.countQuadruplets(arg), expected)
+
+    def test_numberOfWeakCharacters(self):
+        test_patterns = [
+            ([[1, 1], [2, 2], [3, 4], [10, 2], [2, 2], [9, 1], [8, 1], [7, 1], [12, 3]], 7),
+            ([[5, 5], [6, 3], [3, 6]], 0),
+            ([[2, 2], [3, 3]], 1),
+            ([[1, 5], [10, 4], [4, 3]], 1),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.numberOfWeakCharacters(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
