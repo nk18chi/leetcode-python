e0ee52f61bc57ba8bcaa0fc2617b525c5592f543
Naoki Mita <naoki.mita18@gmail.com>
2021-09-12 12:08:49 -0700

    add: weekly contest 258

---
+++ solutions/_contest/weekly_258/index.py
@@ -0,0 +1,25 @@
+from typing import List, Set, Dict, Tuple, Deque
+from collections import deque, defaultdict
+from solutions._class.tree_node import TreeNode
+import math
+
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        index: int = 0
+        for i in range(len(word)):
+            if word[i] == ch:
+                index = i
+                break
+        return word[:index + 1][::-1] + word[index + 1:]
+
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        res: int = 0
+        countMap: Dict[Tuple[int, int], int] = {}
+        for rectangle in rectangles:
+            gcd: int = math.gcd(rectangle[0], rectangle[1])
+            key: Tuple[int, int] = (rectangle[0] // gcd, rectangle[1] // gcd)
+            currentN: int = countMap.get(key, 0)
+            res += currentN
+            countMap[key] = currentN + 1
+        return res
---
+++ solutions/_contest/weekly_258/test.py
@@ -0,0 +1,32 @@
+import unittest
+import solutions._contest.weekly_258.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_reversePrefix(self):
+        test_patterns = [
+            ("abcdefd", "d", "dcbaefd"),
+            ("xyxzxe", "z", "zxyxxe"),
+            ("abcd", "z", "abcd"),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.reversePrefix(arg1, arg2), expected)
+
+    def test_interchangeableRectangles(self):
+        test_patterns = [
+            ([[4, 8], [3, 6], [10, 20], [15, 30]], 6),
+            ([[4, 5], [7, 8]], 0),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.interchangeableRectangles(arg1), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
