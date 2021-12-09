5ad4ea4cac86d22e9d9bbce72b9660bde483d552
Naoki Mita <naoki.mita18@gmail.com>
2021-12-08 20:12:06 -0800

    add time/space complexity for 14

--- solutions/longest_common_prefix/index.py
+++ solutions/longest_common_prefix/index.py
@@ -5,6 +5,8 @@ from typing import List
 
 
 class Solution:
+    # Time complexity: O(S) S is the sum of all letters in given words
+    # Space complexity: O(1)
     def longestCommonPrefix(self, strs: List[str]) -> str:
         for i in range(len(strs[0])):
             cur: str = strs[0][i]
