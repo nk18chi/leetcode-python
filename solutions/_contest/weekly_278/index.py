f6affe00105e9986cd7b59e344dff197323b5a50
Naoki Mita <naoki.mita18@gmail.com>
2022-01-30 16:28:18 -0800

    add contest 278

---
+++ solutions/_contest/weekly_278/index.py
@@ -0,0 +1,45 @@
+from typing import List, Set
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        sort: List[int] = sorted(nums)
+        for n in sort:
+            if n == original:
+                original *= 2
+            elif n > original:
+                break
+        return original
+
+    def maxScoreIndices(self, nums: List[int]) -> List[int]:
+        res = [0]
+        leftSum: int = 0
+        rightSum: int = sum(1 for n in nums if n == 1)
+        currentMax: int = leftSum + rightSum
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                leftSum += 1
+            else:
+                rightSum -= 1
+            if currentMax < (leftSum + rightSum):
+                res = [i + 1]
+                currentMax = leftSum + rightSum
+            elif currentMax == (leftSum + rightSum):
+                res.append(i + 1)
+        return res
+
+    def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
+        def val(c):
+            return ord(c) - ord('a') + 1
+
+        res = n = len(s)
+        pk = pow(power, k, modulo)
+        cur = 0
+
+        for i in range(n - 1, -1, -1):
+            cur = (cur * power + val(s[i])) % modulo
+            if i + k < n:
+                cur = (cur - val(s[i + k]) * pk) % modulo
+            if cur == hashValue:
+                res = i
+        return s[res: res + k]
---
+++ solutions/_contest/weekly_278/test.py
@@ -0,0 +1,48 @@
+import unittest
+import solutions._contest.weekly_278.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_findFinalValue(self):
+        test_patterns = [
+            ([4, 7, 1, 16, 1, 2, 7, 13], 1, 8),
+            ([8, 19, 2, 15, 3], 2, 4),
+            ([2], 2, 4),
+            ([5, 3, 6, 1, 12], 3, 24),
+            ([2, 7, 9], 4, 4),
+            ([8, 19, 4, 2, 15, 3], 2, 16),
+            ([161, 28, 640, 264, 81, 561, 320, 2, 61, 244, 183, 108, 773, 61, 976, 122, 988, 2, 370, 392, 488, 375, 349, 432, 713, 563], 61, 1952),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.findFinalValue(arg1, arg2), expected)
+
+    def test_maxScoreIndices(self):
+        test_patterns = [
+            ([0, 0, 1, 0], [2, 4]),
+            ([0, 0, 0], [3]),
+            ([1, 1], [0]),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.maxScoreIndices(arg1), expected)
+
+    def test_subStrHash(self):
+        test_patterns = [
+            ("leetcode", 7, 20, 2, 0, "ee"),
+            ("fbxzaad", 31, 100, 3, 32, "fbx")
+        ]
+
+        for i, (arg1, arg2, arg3, arg4, arg5, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.subStrHash(arg1, arg2, arg3, arg4, arg5), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
