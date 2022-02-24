1ae0682967c64311049f28a3548b2c54c2ec0a57
Naoki Mita <naoki.mita18@gmail.com>
2022-02-23 20:14:26 -0800

    add: job interview

---
+++ solutions/_ji/az/index.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def maxSubarrayLength(self, badges):
+        count: int = 0
+        pos: List[int] = [len(badges) - 1, 0]
+        for i in range(len(badges)):
+            if badges[i] == -1:
+                pos[0] = min(pos[0], i)
+                pos[1] = max(pos[1], i)
+                count += 1
+        if count % 2 == 0:
+            return len(badges)
+        return max(len(badges[pos[0] + 1:]), len(badges[:pos[1]]))
+
+    def getImbalanceRank(self, ranks):
+        count: int = 0
+        for i in range(len(ranks)):
+            target: int = ranks[i]
+            leftLen: int = 0
+            leftLow: int = 0
+            l: int = i - 1
+            while 0 <= l and ranks[l] != target + 1:
+                if ranks[l] < target and leftLen == leftLow:
+                    leftLow += 1
+                leftLen += 1
+                l -= 1
+
+            rightLen: int = 0
+            rightLow: int = 0
+            r: int = i + 1
+            while r < len(ranks) and ranks[r] != target + 1:
+                if ranks[r] < target and rightLen == rightLow:
+                    rightLow += 1
+                rightLen += 1
+                r += 1
+
+            count += (leftLen - leftLow) * (rightLen - rightLow) + (leftLen - leftLow) * (rightLow + 1) + (rightLen - rightLow) * (leftLow + 1)
+
+        return count
---
+++ solutions/_ji/az/test.py
@@ -0,0 +1,36 @@
+import unittest
+import solutions._contest.weekly_281.index as main
+from solutions._class.list_node import ListNode, createListNode, getListNode
+
+
+class Test(unittest.TestCase):
+    def test_maxSubarrayLength(self):
+        test_patterns = [
+            ([-1, 1, 1, 1], 3),
+            ([1, -1, -1, -1, 1, 1], 4),
+            ([1, -1, 1, -1], 4),
+            ([1, 1, 1, 1], 4),
+            ([-1, 1, 1, -1], 4),
+            ([-1, -1, 1, -1], 3),
+            ([1, 1, 1, -1, 1], 3),
+            ([1, 1, 1, -1, 1, -1, -1], 6),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.maxSubarrayLength(arg1), expected)
+
+    def test_getImbalanceRank(self):
+        test_patterns = [
+            ([4, 1, 3, 2], 3),
+        ]
+
+        for i, (arg1, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.getImbalanceRank(arg1), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
