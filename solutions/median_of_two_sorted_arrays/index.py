7005c349e627531991dfcb90a0a0479d8519496f
Naoki Mita <naoki.mita18@gmail.com>
2022-03-24 20:43:06 -0700

    add 4. Median of Two Sorted Arrays

---
+++ solutions/median_of_two_sorted_arrays/index.py
@@ -0,0 +1,58 @@
+# 4. Median of Two Sorted Arrays
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+from typing import List
+
+
+class Solution:
+    # # Time complexity: O(m + n)
+    # # Space complexity: O(1)
+    # def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    #     l1: int = 0
+    #     l2: int = 0
+    #     history: List[int] = [0, 0]
+    #     totalSum: int = len(nums1) + len(nums2)
+    #     while l1 + l2 <= (totalSum // 2):
+    #         history[0] = history[1]
+    #         if l1 == len(nums1):
+    #             history[1] = nums2[l2]
+    #             l2 += 1
+    #         elif l2 == len(nums2):
+    #             history[1] = nums1[l1]
+    #             l1 += 1
+    #         elif nums1[l1] < nums2[l2]:
+    #             history[1] = nums1[l1]
+    #             l1 += 1
+    #         else:
+    #             history[1] = nums2[l2]
+    #             l2 += 1
+    #     return sum(history) / 2 if totalSum % 2 == 0 else history[1]
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+
+        left_size = (m + n + 1) // 2
+        start = 0
+        end = m
+        is_even = ((m + n) % 2) == 0
+        while start <= end:
+            a_part = (start + end) // 2
+            b_part = left_size - a_part
+
+            aleftmax = float("-inf") if a_part == 0 else nums1[a_part - 1]
+            arightmin = float("inf") if a_part == m else nums1[a_part]
+            bleftmax = float("-inf") if b_part == 0 else nums2[b_part - 1]
+            brightmin = float("inf") if b_part == n else nums2[b_part]
+
+            if aleftmax <= brightmin and bleftmax <= arightmin:
+                if not is_even:
+                    return max(aleftmax, bleftmax)
+                else:
+                    return (max(aleftmax, bleftmax) + min(arightmin, brightmin)) / 2
+            elif aleftmax > brightmin:
+                end = a_part - 1
+            elif bleftmax > arightmin:
+                start = a_part + 1
---
+++ solutions/median_of_two_sorted_arrays/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.median_of_two_sorted_arrays.index as main
+
+
+class Test(unittest.TestCase):
+    def test_findMedianSortedArrays(self):
+        test_patterns = [
+            ([1, 2], [3, 4], 2.50000),
+            ([3, 4], [1, 2], 2.50000),
+            ([1, 3], [2], 2.00000),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.findMedianSortedArrays(arg1, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
