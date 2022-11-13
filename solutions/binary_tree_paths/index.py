3403bce4000c1b5d67cfaa046cbc47cf18233287
Naoki Mita <naoki.mita18@gmail.com>
2022-11-13 14:24:24 -0800

    add 257. Binary Tree Paths

---
+++ solutions/binary_tree_paths/index.py
@@ -0,0 +1,25 @@
+# 257. Binary Tree Paths
+# https://leetcode.com/problems/binary-tree-paths/
+
+from typing import List, Optional
+from solutions._class.tree_node import TreeNode
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        def dfs(self, node: Optional[TreeNode], cur: List[str]):
+            if node is None or node.val is None:
+                return
+            if node.left is None and node.right is None:
+                cur.append(str(node.val))
+                self.res.append('->'.join(cur))
+                cur.pop()
+                return
+            cur.append(str(node.val))
+            dfs(self, node.left, cur)
+            dfs(self, node.right, cur)
+            cur.pop()
+
+        self.res: List[str] = []
+        dfs(self, root, [])
+        return self.res
---
+++ solutions/binary_tree_paths/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.binary_tree_paths.index as main
+from solutions._class.tree_node import TreeNode, createTreeNode
+
+
+class Test(unittest.TestCase):
+    def test_binaryTreePaths(self):
+        test_patterns = [
+            ([1, 2, 3, None, 5], ["1->2->5", "1->3"]),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                tree: TreeNode = createTreeNode(arg)
+                self.assertEqual(s.binaryTreePaths(tree), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
