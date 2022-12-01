f869359e31d03546d401c2789f94b9807678312a
Naoki Mita <naoki.mita18@gmail.com>
2022-11-30 17:36:59 -0800

    add 17. Letter Combinations of a Phone Number

---
+++ solutions/letter_combinations_of_a_phone_number/index.py
@@ -0,0 +1,32 @@
+# 17. Letter Combinations of a Phone Number
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+from typing import List, Dict
+
+
+class Solution:
+    # Time complexity: O(4 ^ N)
+    # Space complexity: O(1)
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        phoneMap: Dict[str, str] = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+
+        def dfs(self, digits: str, path: str):
+            if len(digits) == 0:
+                return self.res.append(path)
+            for n in phoneMap[digits[0]]:
+                dfs(self, digits[1:], path + n)
+
+        self.res: List[str] = []
+        dfs(self, digits, "")
+        return self.res
---
+++ solutions/letter_combinations_of_a_phone_number/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.letter_combinations_of_a_phone_number.index as main
+
+
+class Test(unittest.TestCase):
+    def test_letterCombinations(self):
+        test_patterns = [
+            ('', []),
+            ('2', ["a", "b", "c"]),
+            ('23', ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.letterCombinations(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
