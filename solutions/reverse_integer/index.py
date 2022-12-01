c00fe8fa460ed72197049ffc4062fce159363860
Naoki Mita <naoki.mita18@gmail.com>
2022-11-30 16:49:48 -0800

    add 7. Reverse Integer

---
+++ solutions/reverse_integer/index.py
@@ -0,0 +1,16 @@
+# 7. Reverse Integer
+# https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+    # Time complexity: O(1)
+    # Space complexity: O(1)
+    def reverse(self, x: int) -> int:
+        sign: int = 1
+        res: int = 0
+        if x < 0:
+            sign = -1
+            x *= -1
+        while x > 0:
+            res = res * 10 + x % 10
+            x //= 10
+        return res * sign if res <= pow(2, 31) else 0
---
+++ solutions/reverse_integer/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.reverse_integer.index as main
+
+
+class Test(unittest.TestCase):
+    def test_reverse(self):
+        test_patterns = [
+            (123, 321),
+            (-123, -321),
+            (1534236469, 0),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.reverse(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
