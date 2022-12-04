4e5268e42dcd251562cf64f2380e0c3b343b69b0
Naoki Mita <naoki.mita18@gmail.com>
2022-12-04 14:51:10 -0800

    improve 168. Excel Sheet Column Title

--- solutions/excel_sheet_column_title/index.py
+++ solutions/excel_sheet_column_title/index.py
@@ -5,14 +5,23 @@
 
 
 class Solution:
-    def convertToTitle(self, n: int) -> str:
-        list = []
-        while (0 < n):
-            remainder = n % 26
-            remainder = 26 if remainder == 0 else remainder
-            list = [chr(ord("@") + remainder)] + list
-            n -= remainder
-            import math
-            n = math.floor(n / 26)
+    # def convertToTitle(self, n: int) -> str:
+    #     list = []
+    #     while (0 < n):
+    #         remainder = n % 26
+    #         remainder = 26 if remainder == 0 else remainder
+    #         list = [chr(ord("@") + remainder)] + list
+    #         n -= remainder
+    #         import math
+    #         n = math.floor(n / 26)
 
-        return "".join(list)
+    #     return "".join(list)
+
+    def convertToTitle(self, columnNumber: int) -> str:
+        res: str = ""
+        while columnNumber > 0:
+            columnNumber -= 1
+            mod: int = columnNumber % 26
+            res += chr(mod + 65)
+            columnNumber //= 26
+        return res[::-1]
