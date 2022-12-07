90731b8bb75c2f271de4109677f0c4cd074964d5
Naoki Mita <naoki.mita18@gmail.com>
2022-12-06 21:16:07 -0800

    add 54. Spiral Matrix

---
+++ solutions/spiral_matrix/index.py
@@ -0,0 +1,28 @@
+# 54. Spiral Matrix
+# https://leetcode.com/problems/spiral-matrix
+
+from typing import List, Tuple
+
+
+class Solution:
+    # Time complexity: O(MN)
+    # Space complexity: O(1)
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        self.directions: List[Tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        self.res: List[int] = []
+
+        def dfs(self, row: int, col: int, directionIndex: int):
+            if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[row]) or matrix[row][col] == 777:
+                return
+            self.res.append(matrix[row][col])
+            matrix[row][col] = 777
+
+            nextRow: int = row + self.directions[directionIndex][0]
+            nextCol: int = col + self.directions[directionIndex][1]
+            if nextRow < 0 or nextRow >= len(matrix) or nextCol < 0 or nextCol >= len(matrix[nextRow]) or matrix[nextRow][nextCol] == 777:
+                directionIndex = (directionIndex + 1) % len(self.directions)
+            nextRow = row + self.directions[directionIndex][0]
+            nextCol = col + self.directions[directionIndex][1]
+            dfs(self, nextRow, nextCol, directionIndex)
+        dfs(self, 0, 0, 0)
+        return self.res
---
+++ solutions/spiral_matrix/test.py
@@ -0,0 +1,19 @@
+import unittest
+import solutions.spiral_matrix.index as main
+
+
+class Test(unittest.TestCase):
+    def test_spiralOrder(self):
+        test_patterns = [
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+            ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.spiralOrder(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
