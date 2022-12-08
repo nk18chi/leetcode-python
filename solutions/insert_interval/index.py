1ce0e7d9b3f00eb21e918fa09f9e9a600afe938b
Naoki Mita <naoki.mita18@gmail.com>
2022-12-07 21:54:59 -0800

    add 57. Insert Interval

---
+++ solutions/insert_interval/index.py
@@ -0,0 +1,26 @@
+# 57. Insert Interval
+# https://leetcode.com/problems/insert-interval/
+
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(N)
+    # Space complexity: O(1)
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res: List[List[int]] = []
+        start: int = newInterval[0]
+        end: int = newInterval[1]
+        i = 0
+        for i in range(len(intervals)):
+            if intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+            elif newInterval[1] < intervals[i][0]:
+                i -= 1
+                break
+            else:
+                start = min(start, intervals[i][0])
+                end = max(end, intervals[i][1])
+        res.append([start, end])
+        res += intervals[i + 1:]
+        return res
---
+++ solutions/insert_interval/test.py
@@ -0,0 +1,21 @@
+import unittest
+import solutions.insert_interval.index as main
+
+
+class Test(unittest.TestCase):
+    def test_insert(self):
+        test_patterns = [
+            ([[1, 3], [6, 9]], [2, 5], [[1, 5], [6, 9]]),
+            ([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8], [[1, 2], [3, 10], [12, 16]]),
+            ([[2, 3], [6, 9]], [0, 1], [[0, 1], [2, 3], [6, 9]]),
+            ([], [2, 5], [[2, 5]]),
+        ]
+
+        for i, (arg1, arg2, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.insert(arg1, arg2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
