8941a1aebb759b365af9975c02c454ccba1e39c3
Naoki <42604585+nk18chi@users.noreply.github.com>
2022-12-13 10:51:11 -0800

    add 994. Rotting Oranges (#4)

---
+++ solutions/rotting_oranges/test.py
@@ -0,0 +1,20 @@
+import unittest
+import solutions.rotting_oranges.index as main
+
+
+class Test(unittest.TestCase):
+    def test_orangesRotting(self):
+        test_patterns = [
+            ([[2, 1, 1], [1, 1, 0], [0, 1, 1]], 4),
+            ([[2, 1, 1], [0, 1, 1], [1, 0, 1]], -1),
+            ([[0, 2]], 0),
+        ]
+
+        for i, (arg, expected) in enumerate(test_patterns):
+            with self.subTest(test=i):
+                s = main.Solution()
+                self.assertEqual(s.orangesRotting(arg), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
