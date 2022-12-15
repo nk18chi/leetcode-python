4961beebc026ca14152ec956771299657acfdf46
Naoki <42604585+nk18chi@users.noreply.github.com>
2022-12-15 14:19:45 -0800

    use dynamic programing to solve problem 39 (#6)

--- solutions/combination_sum/test.py
+++ solutions/combination_sum/test.py
@@ -5,8 +5,10 @@ import solutions.combination_sum.index as main
 class Test(unittest.TestCase):
     def test_combinationSum(self):
         test_patterns = [
-            ([2, 4, 3, 6], 8,
-             [[2, 2, 2, 2], [2, 2, 4], [2, 3, 3], [2, 6], [4, 4]]),
+            ([2, 3, 6, 7], 7, [[2, 2, 3], [7]]),
+            ([2, 3, 5], 8, [[2, 2, 2, 2], [2, 3, 3], [3, 5]]),
+            ([2], 1, []),
+            ([2, 4, 3, 6], 8, [[2, 2, 2, 2], [2, 2, 4], [4, 4], [2, 3, 3], [2, 6]]),
         ]
 
         for i, (arg1, arg2, expected) in enumerate(test_patterns):
