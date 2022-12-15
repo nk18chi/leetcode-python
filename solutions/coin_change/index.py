7c9920ee6bd044b3211c66175a1e2de1b09e23a9
Naoki <42604585+nk18chi@users.noreply.github.com>
2022-12-14 22:47:02 -0800

    322. Coin Change (#5)

---
+++ solutions/coin_change/index.py
@@ -0,0 +1,21 @@
+# 322. Coin Change
+# https://leetcode.com/problems/coin-change/
+
+from typing import List
+import math
+
+
+class Solution:
+    # Time complexity: O(N * amount)
+    # Space complexity: O(N)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        count: int = 0
+        dp: List[float] = [float('inf') for _ in range(amount + 1)]
+        dp[0] = 0
+        while count < amount:
+            count += 1
+            for coin in coins:
+                if count - coin < 0:
+                    continue
+                dp[count] = min(dp[count - coin] + 1, dp[count])
+        return -1 if math.isinf(dp[amount]) else int(dp[amount])
