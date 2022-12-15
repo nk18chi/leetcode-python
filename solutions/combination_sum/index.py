e36f98c86b4ee6bea48b3197cee97edadd471cb7
Naoki Mita <naoki.mita18@gmail.com>
2022-12-15 14:18:46 -0800

    use dynamic programing to solve problem 39

--- solutions/combination_sum/index.py
+++ solutions/combination_sum/index.py
@@ -2,22 +2,20 @@
 # https://leetcode.com/problems/combination-sum/
 
 from typing import List
+import copy
 
 
 class Solution:
-    def combinationSum(self,
-                       candidates: List[int],
-                       target: int) -> List[List[int]]:
-        def dfs(self, target, index, list):
-            if target == 0:
-                self.res.append(list)
-                return
-            for i in range(index, len(candidates)):
-                if target - candidates[i] < 0:
-                    break
-                dfs(self, target - candidates[i], i, list + [candidates[i]])
-
-        self.res = []
-        candidates.sort()
-        dfs(self, target, 0, [])
-        return self.res
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp: List[List[List[int]]] = [[] for _ in range(target + 1)]
+        for candidate in candidates:
+            for i in range(candidate, target + 1):
+                if i - candidate < 0:
+                    continue
+                if i - candidate == 0:
+                    dp[i].append([candidate])
+                for sub in dp[i - candidate]:
+                    p: List[int] = copy.deepcopy(sub)
+                    p.append(candidate)
+                    dp[i].append(p)
+        return dp[target]
